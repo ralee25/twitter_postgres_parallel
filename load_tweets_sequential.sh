9541583a697c7293d2546d5cbc324904ced2bd56
Rachel Lee <112450184+ralee25@users.noreply.github.com>
2025-04-23 01:30:22 -0700

    Update load_tweets_sequential.sh

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -17,7 +17,7 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
-    python3 load_tweets.py --db "postgresql://postgres:pass@localhost:1303" --inputs "$file"
+    ./load_tweets.py --db "postgresql://postgres:pass@localhost:1303" --inputs "$file"
 done
 
 echo '================================================================================'
